52eb4bbec10f941812bb79bc4996bdededdbcfa4
Jaron <jaron_zijlstra@outlook.com>
2024-02-13 22:06:51 +0100

    chore(UserService): move dependency to service

--- app/api/v1/presentation/endpoints/users.py
+++ app/api/v1/presentation/endpoints/users.py
@@ -1,22 +1,18 @@
-from typing import Annotated
-
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from mangum import Mangum
 
 from ...app import PixelPizzaAPI
 from ..model.user import Registration, User
 from ..mapper.user import map_registration_to_user_domain, map_user_domain_to_user
-from ...application.service.user import UserService
+from ...application.service.user import UserServiceDep
 
 router = APIRouter(prefix="/users")
-
-type UserServiceDep = Annotated[UserService, Depends()]
 
 
 @router.post("/", status_code=201)
 async def create_user(
     registration: Registration,
-    user_service: Annotated[UserService, Depends()]
+    user_service: UserServiceDep
 ) -> User:
     user_domain = map_registration_to_user_domain(registration)
     user_service.create_user(user_domain)
